eb6cb474793bb9516160a85aa740e7d0d3eafc77
Alif Satriya Wicaksono <alifsw2006@gmail.com>
2026-06-19 21:31:36 +0700

    feat: menambahkan konfigurasi Wokwi untuk sensor air dan cuaca

---
+++ iot/Sensor Air/sketch.ino
@@ -0,0 +1,93 @@
+#include <WiFi.h>
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+
+const char* ssid = "Wokwi-GUEST";
+const char* mqtt_server = "broker.hivemq.com";
+const int mqtt_port = 1883;
+const char* DEVICE_ID = "Sensor-banjir1";
+
+#define TRIG_PIN 21
+#define ECHO_PIN 19
+#define POT_PIN 34
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+unsigned long lastMsg = 0;
+
+void publishSensorData() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH);
+
+  float distance_m = (duration * 0.034/2) / 100.0;
+
+  float waterLevel_m = 5.0 - distance_m;
+  if (waterLevel_m < 0) {
+    waterLevel_m = 0;
+  }
+
+  int raw_pot = analogRead(POT_PIN);
+
+  float soilMoisture_pct = (raw_pot / 4095.0) * 100.0;
+
+  StaticJsonDocument<300> doc;
+  doc["idNode"] = 1;
+  doc["WaterLevel_m"] = waterLevel_m;
+  doc["SoilMoisture_pct"] = soilMoisture_pct;
+
+  char payload[300];
+  serializeJson(doc, payload);
+
+  client.publish("kelompok2/sensors/sungai", payload, true);
+  Serial.println("Node 1 Mengirim: " + String(payload));
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Mencoba koneksi MQTT Node 1...");
+    if (client.connect(DEVICE_ID)) {
+      Serial.println("TERHUBUNG");
+    } else {
+      Serial.print("gagal, reconnect=");
+      Serial.print(client.state());
+      Serial.println(" coba lagi dalam 5 detik");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(POT_PIN, INPUT);
+
+  WiFi.begin(ssid, "");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(("\nWiFi Terhubung"));
+  
+  client.setServer(mqtt_server, mqtt_port);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now -lastMsg > 5000) {
+    lastMsg = now;
+    publishSensorData();
+  }
+}
---
+++ iot/Sensor Cuaca/sketch.ino
@@ -0,0 +1,98 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "Wokwi-GUEST";
+const char* mqtt_server = "broker.hivemq.com";
+const int mqtt_port = 1883;
+const char* DEVICE_ID = "Sensor-banjir-cuaca2";
+
+#define POTENTIO_PIN 34 
+
+DHT dht(4, DHT22);
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+unsigned long lastMsg = 0;
+
+void publishSensorData() {
+  float t_avg = dht.readTemperature();
+  float rh_avg = dht.readHumidity();
+
+  int raw_pot = analogRead(POTENTIO_PIN);
+  float rainfall = (raw_pot / 4095.0) * 50.0; 
+
+  if (isnan(t_avg) || isnan(rh_avg)) {
+    Serial.println("Gagal membaca DHT22");
+    return;
+  }
+
+  float tn = t_avg - 2.0;
+  float tx = t_avg + 3.0;
+  float ss = 5.0 + (random(-10, 11) / 10.0);
+  float ff_x = 5.0 + (random(-10, 11) / 10.0);
+  float ddd_x = 193.5 + (random(-200, 201) / 10.0);
+  float ff_avg = 2.3 + (random(-10, 11) / 10.0);
+
+  StaticJsonDocument<1024> doc; 
+  doc["idNode"] = 2;
+  doc["Rainfall_mm"] = rainfall;
+  doc["Tn"] = tn;
+  doc["Tx"] = tx;
+  doc["Tavg"] = t_avg;
+  doc["RH_avg"] = rh_avg;
+  doc["ss"] = ss;
+  doc["ff_x"] = ff_x;
+  doc["ddd_x"] = ddd_x;
+  doc["ff_avg"] = ff_avg;
+
+  char payload[1024];
+  serializeJson(doc, payload);
+
+  client.publish("kelompok2/sensors/cuaca", payload, true);
+  Serial.println("Node 2 Mengirim: " + String(payload));
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Mencoba koneksi MQTT...");
+    if (client.connect(DEVICE_ID)) {
+      Serial.println("TERHUBUNG!");
+    } else {
+      Serial.print("gagal, reconnect=");
+      Serial.print(client.state());
+      Serial.println(" coba lagi dalam 5 detik");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  pinMode(POTENTIO_PIN, INPUT);
+  dht.begin();
+
+  WiFi.begin(ssid, "");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi Terhubung!");
+
+  client.setServer(mqtt_server, mqtt_port);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    publishSensorData();
+  }
+}
